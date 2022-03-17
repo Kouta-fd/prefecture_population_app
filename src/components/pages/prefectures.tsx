f2c488c00eafc9643ebbc9d7579cd82361bb2265
Kouta-fd <kotafd0124@icloud.com>
2022-03-17 22:39:06 +0900

    feat:featuresPage

---
+++ src/components/pages/prefectures.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../module/Header';
+import CheckList from '../module/checkList';
+
+export const Prefectures: React.FC = () => (
+  <>
+    <Header title="タイトル" />
+    <CheckList secTitle="都道府県" />
+  </>
+);
+
+export default Prefectures;
