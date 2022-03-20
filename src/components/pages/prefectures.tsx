4b2c0e3dd1416f3ea888c72f24ac34fcfef73d3d
Kouta-fd <kotafd0124@icloud.com>
2022-03-20 15:22:23 +0900

    feat:mockdata作成、checkListをモックデータで表示

--- src/components/pages/prefectures.tsx
+++ src/components/pages/prefectures.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import Header from '../module/Header';
 import CheckList from '../module/checkList';
 import Graph from '../module/Graph';
+import { prefectures } from '../../mock_data/prefectures';
+import { population } from '../../mock_data/population';
+
+const handleCheck = (prefName: string, prefCode: number, check: boolean) => {
+  console.log(prefName);
+  console.log(prefCode);
+  console.log(check);
+};
 
 export const Prefectures: React.FC = () => (
   <>
     <Header title="タイトル" />
-    <CheckList secTitle="都道府県" />
-    <Graph />
+    <CheckList secTitle="都道府県" prefectures={prefectures.result} onChange={handleCheck} />
+    <Graph population={population} />
   </>
 );
 
